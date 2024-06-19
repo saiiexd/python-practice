c1bb9fbd0e7c2ed23f76d042a0d5620f40bf17dd
saiie.xd <saivenkat262005@gmail.com>
2024-06-19 17:41:34 +0530

    completed strings

---
+++ chapter - 3/strings.py
@@ -0,0 +1,23 @@
+a  = "sai venkat"
+b  = 'sai venkat'
+c = '''saivenkat'''
+
+#string is immutable
+
+#to find length of string 
+e = "sai venkat"
+print(len(e))
+
+#string slicing
+text = "Hello, World!"
+print(text[0:5])  # Output: Hello
+
+#omitting start or end
+print(text[:5])   # Output: Hello
+print(text[7:])   # Output: World!
+
+
+#negative slicing
+print(text[-6:])  # Output: World!
+print(text[-6:-1])  # Output: World
+
