315473c932ec3541f01c2c6b89118b79375f58e7
saiie.xd <saivenkat262005@gmail.com>
2024-06-23 11:01:22 +0530

    completed tuples

---
+++ chapter - 4/tuple.py
@@ -0,0 +1,6 @@
+a = (1,45,35,3463,356, False, "Rohan", "shivam") #empty tuple
+print(type(a))
+print(a)
+
+#tuple is immutable
+
---
+++ chapter - 4/tuplemethods.py
@@ -0,0 +1,10 @@
+a = (1,45,35,3463,356, False, "Rohan", "shivam") #empty tuple
+print(a)
+
+no = a.count(45)
+print(no)
+
+i = a.index(35) #shows index 
+print(i)
+
+
