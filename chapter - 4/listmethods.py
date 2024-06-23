f11e6b1a26223415e248c64f37112690635c5311
saiie.xd <saivenkat262005@gmail.com>
2024-06-23 10:54:48 +0530

    completed list

---
+++ chapter - 4/listmethods.py
@@ -0,0 +1,18 @@
+friends = ["apple", "orange", 5, 345.456, "Akash"]
+friends[0] = "Grapes"
+friends.append("sai venkat") #add at end
+print(friends)
+
+l1 = [1,4,345,534,6356,2]
+l1.sort() #sorts the list
+print(l1)
+
+l1.reverse() #reverses the list 
+print(l1)
+
+l1.insert(3, 324524) #inserts object in the list 
+print(l1.pop(4)) #pops out an object 
+print(l1)
+
+print(l1.remove(345)) #removes an element 
+print(l1)
---
+++ chapter - 4/lists.py
@@ -0,0 +1,10 @@
+friends = ["apple", "orange", 5, 345.456, "Akash"]
+
+#strings are immutable
+#we can change strings in lists
+#lists are fully mutable
+
+
+friends[0] = "Grapes"
+print(friends)
+print(friends[1:4])
